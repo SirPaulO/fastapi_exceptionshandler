d34337eea209c31c3665da2316eb61015bfd020d
SirPaulO <me@sirpauloliver.com>
2023-10-04 12:40:09 -0300

    :hammer: Renaming and import in init

--- fastapi_exceptionshandler/__init__.py
+++ fastapi_exceptionshandler/__init__.py
@@ -1,10 +1,6 @@
 from .api_exception import APIError, APIException
 from .api_exception_handler import APIExceptionHandler
 from .api_exception_manager import APIExceptionManager
+from .api_exception_middleware import APIExceptionMiddleware
 
-__all__ = [
-    "APIException",
-    "APIError",
-    "APIExceptionManager",
-    "APIExceptionHandler",
-]
+__all__ = ["APIException", "APIError", "APIExceptionManager", "APIExceptionHandler", "APIExceptionMiddleware"]
